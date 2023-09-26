e84f699f3a5efb691e51c32af6878c3ad1b0f0de
stevennchristopher <stchrist03@gmail.com>
2023-09-26 22:33:56 +0700

    home_cerbung

---
+++ app/src/main/java/com/example/midtermprojectcerbung/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.example.midtermprojectcerbung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.midtermprojectcerbung.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+//        setContentView(view)
+        setContentView(R.layout.activity_home)
+    }
+}
