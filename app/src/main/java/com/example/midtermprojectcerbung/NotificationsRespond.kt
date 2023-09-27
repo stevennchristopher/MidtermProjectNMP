0b8b8917042aef27632baf00598fc5cf9d845ee7
stevennchristopher <stchrist03@gmail.com>
2023-09-27 13:27:46 +0700

    add 13 page

---
+++ app/src/main/java/com/example/midtermprojectcerbung/NotificationsRespond.kt
@@ -0,0 +1,11 @@
+package com.example.midtermprojectcerbung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationsRespond : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifications_respond)
+    }
+}
---
+++ app/src/main/java/com/example/midtermprojectcerbung/ReadRestrict.kt
@@ -0,0 +1,11 @@
+package com.example.midtermprojectcerbung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadRestrict : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read_restrict)
+    }
+}
