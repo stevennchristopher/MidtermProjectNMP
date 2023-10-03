1d043b6d9883797cf16de4dcbf1c63fd7392051f
stevennchristopher <stchrist03@gmail.com>
2023-10-04 01:08:57 +0700

    create 1 done

--- app/src/main/java/com/example/hadifamilycerbung/Create1Activity.kt
+++ app/src/main/java/com/example/hadifamilycerbung/Create1Activity.kt
@@ -2,15 +2,23 @@ package com.example.hadifamilycerbung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.hadifamilycerbung.databinding.ActivityCreate1Binding
 import com.example.hadifamilycerbung.databinding.ActivityHomeBinding
 
 class Create1Activity : AppCompatActivity() {
     private lateinit var binding: ActivityCreate1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreate1Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val genres = resources.getStringArray(R.array.genres)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_genre, genres)
+        binding.autoCompleteTxtInputImg.setAdapter(arrayAdapter)
+
+
     }
 }
