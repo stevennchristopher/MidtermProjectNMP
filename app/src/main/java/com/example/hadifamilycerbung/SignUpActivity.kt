4f84ae47a78efedaf6a2d7d34e20906fc5707ee5
melissaveronica <s160821004@student.ubaya.ac.id>
2023-10-05 20:37:55 +0700

    signup

--- app/src/main/java/com/example/hadifamilycerbung/SignUpActivity.kt
+++ app/src/main/java/com/example/hadifamilycerbung/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hadifamilycerbung
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.hadifamilycerbung.databinding.ActivitySignInBinding
 import com.example.hadifamilycerbung.databinding.ActivitySignUpBinding
 
@@ -12,5 +14,52 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        var counter = 0
+        var countData = 0;
+
+        binding.buttonSignUp.setOnClickListener{
+            val username = binding.txtInputUsernameNew.text.toString()
+            val password = binding.txtInputPasswordNew.text.toString()
+            val url = binding.txtInputProfilePictUrl.text.toString()
+            val retypePassword = binding.txtInputPasswordConfirmNew.text.toString()
+
+            for (user in Global.userData){
+                if(user.username == username){
+                    counter++
+                }
+                else
+                {
+                    countData++
+                }
+            }
+
+            if (counter == 0)
+            {
+                if (password == retypePassword)
+                {
+                    val newUser = User((countData+1),username,url,password)
+                    Global.userData.add(newUser)
+                    Toast.makeText(this, "Sign Up Success.", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, SignInActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else
+                {
+                    Toast.makeText(this, "Password and retype password don't match.", Toast.LENGTH_LONG).show()
+                    counter = 0
+                    countData = 0
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "Username unavailable.", Toast.LENGTH_LONG).show()
+                counter = 0
+                countData = 0
+            }
+
+
+        }
     }
+
 }
