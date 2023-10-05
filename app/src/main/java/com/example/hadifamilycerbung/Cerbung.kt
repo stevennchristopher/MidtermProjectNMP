c5d52a84bc86d1a8f3cb8f6ec8fbe234ac51c12a
stevennchristopher <stchrist03@gmail.com>
2023-10-05 20:06:40 +0700

    sign in selesai

--- app/src/main/java/com/example/hadifamilycerbung/Cerbung.kt
+++ app/src/main/java/com/example/hadifamilycerbung/Cerbung.kt
@@ -1,3 +1,4 @@
 package com.example.hadifamilycerbung
 
-data class CerbungHome(val url:String, val title:String, val usernameCreate:String, val number:Int, val thumbs:Int, val description:String)
+data class User(val id:Int, val username:String, val urlProfile:String, val password:String)
+data class CerbungHome(val id:Int, val url:String, val title:String, val usernameCreate:String, val number:Int, val thumbs:Int, val description:String)
