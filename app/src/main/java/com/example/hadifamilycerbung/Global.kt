3053ee9b64fb758bc6017a2e70a1726b77341021
Ryan <ryanhadi82@gmail.com>
2023-10-06 13:54:24 +0700

    ubah nama class & nambah data
    cerbunghome to cerbung
    data yg di home kurang 1 jadi tak tambahin

--- app/src/main/java/com/example/hadifamilycerbung/Global.kt
+++ app/src/main/java/com/example/hadifamilycerbung/Global.kt
@@ -8,14 +8,16 @@ import java.util.Locale
 object Global {
     val cerbungHome =
         arrayListOf(
-            CerbungHome(1, "https://live.staticflickr.com/737/32640476365_906f64ce29_b.jpg", "Rahasia Terkunci di Perpustakaan Kuno", "by NebulaNomad", 13, 5, "\"Rahasia Terkunci di Perpustakaan Kuno\" mengisahkan tentang seorang mahasiswa bernama Alex yang secara tak sengaja menemukan sebuah buku kuno yang misterius di perpustakaan universitasnya. "),
-            CerbungHome(2, "https://live.staticflickr.com/737/32640476365_906f64ce29_b.jpg", "Rahasia Terkunci di Perpustakaan Kuno Chapter 2", "by NebulaNomad", 13, 5, "\"Rahasia Terkunci di Perpustakaan Kuno\" mengisahkan tentang seorang mahasiswa bernama Alex yang secara tak sengaja menemukan sebuah buku kuno yang misterius di perpustakaan universitasnya. ")
+            Cerbung(1, "https://live.staticflickr.com/737/32640476365_906f64ce29_b.jpg", "Rahasia Terkunci di Perpustakaan Kuno", "by NebulaNomad", 13, 5, "\"Rahasia Terkunci di Perpustakaan Kuno\" mengisahkan tentang seorang mahasiswa bernama Alex yang secara tak sengaja menemukan sebuah buku kuno yang misterius di perpustakaan universitasnya. "),
+            Cerbung(2, "https://live.staticflickr.com/737/32640476365_906f64ce29_b.jpg", "Rahasia Terkunci di Perpustakaan Kuno Chapter 2", "by NebulaNomad", 13, 5, "\"Rahasia Terkunci di Perpustakaan Kuno\" mengisahkan tentang seorang mahasiswa bernama Alex yang secara tak sengaja menemukan sebuah buku kuno yang misterius di perpustakaan universitasnya. "),
+            Cerbung(3, "https://coc.guide/static/imgs/troop/barbarian.png", "Rahasia Update Terbaru Clash of Clans", "by ryan", 5, 200, "\"Update Clash of Clans Terbaru\" blablbablalblalblablablbablalblalblablablbablalblalblablablbablalblalblablablbablalblalblablablbablalblalblablablbablalblalblablablbablalblalbla. ")
         )
 
     val userData =
         arrayListOf(
             User(1, "steven", "https://live.staticflickr.com/737/32640476365_906f64ce29_b.jpg", "test123"),
-            User(2, "melissa", "https://live.staticflickr.com/737/32640476365_906f64ce29_b.jpg", "test123")
+            User(2, "melissa", "https://live.staticflickr.com/737/32640476365_906f64ce29_b.jpg", "test123"),
+            User(3, "ryan", "https://coc.guide/static/imgs/troop/barbarian.png", "h")
         )
 
     val cerbungNotification =
