e5139f35a4dc340409cae330dc74fac86e24d56a
melissaveronica <s160821004@student.ubaya.ac.id>
2023-12-26 11:52:57 +0700

    pref

--- app/src/main/java/com/example/hadifamilycerbung/PrefsActivity.kt
+++ app/src/main/java/com/example/hadifamilycerbung/PrefsActivity.kt
@@ -2,6 +2,8 @@ package com.example.hadifamilycerbung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.hadifamilycerbung.databinding.ActivityHomeBinding
 import com.example.hadifamilycerbung.databinding.ActivityPrefsBinding
 
@@ -15,5 +17,34 @@ class PrefsActivity : AppCompatActivity() {
         binding = ActivityPrefsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.apply {
+            // Sembunyikan default ActionBar jika perlu
+            setDisplayShowTitleEnabled(false)
+        }
+        supportActionBar?.title = "Prefs"
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu -> {
+                // Tindakan yang ingin dilakukan saat menu diklik
+                true
+            }
+            R.id.item_notification -> {
+                // Tindakan yang ingin dilakukan saat menu diklik
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
