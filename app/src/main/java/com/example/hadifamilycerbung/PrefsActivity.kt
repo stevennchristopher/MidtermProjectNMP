563e290516c33bef450c0591819ac6ef2d654b69
melissaveronica <s160821004@student.ubaya.ac.id>
2023-12-26 13:30:58 +0700

    prefz

--- app/src/main/java/com/example/hadifamilycerbung/PrefsActivity.kt
+++ app/src/main/java/com/example/hadifamilycerbung/PrefsActivity.kt
@@ -1,50 +1,68 @@
 package com.example.hadifamilycerbung
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.example.hadifamilycerbung.databinding.ActivityHomeBinding
 import com.example.hadifamilycerbung.databinding.ActivityPrefsBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class PrefsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPrefsBinding
+    companion object {
+        val user_login_cerbungHadiFamily = "random_16071239872_user"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        supportActionBar?.hide()
-
         super.onCreate(savedInstanceState)
         binding = ActivityPrefsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        setSupportActionBar(binding.toolbar)
+        val userId = intent.getIntExtra(HomeActivity.user_login_cerbungHadiFamily, 0)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
 
-        supportActionBar?.apply {
-            // Sembunyikan default ActionBar jika perlu
-            setDisplayShowTitleEnabled(false)
-        }
-        supportActionBar?.title = "Prefs"
+        // Find the TextView inside the toolbar and set the title
+        val titleTextView = toolbar.findViewById<TextView>(R.id.title)
+        titleTextView.text = "Prefs"
 
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.item_menu -> {
-                // Tindakan yang ingin dilakukan saat menu diklik
-                true
+        binding.bottomNav.selectedItemId = R.id.itemPref
+        binding.bottomNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.itemHome -> {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    intent.putExtra(HomeActivity.user_login_cerbungHadiFamily, userId)
+                    startActivity(intent)
+                    true
+                }
+                R.id.itemFollowing -> {
+                    val intent = Intent(this, FollowingActivity::class.java)
+                    intent.putExtra(FollowingActivity.user_login_cerbungHadiFamily, userId)
+                    startActivity(intent)
+                    true
+                }
+                R.id.itemCreate -> {
+                    val intent = Intent(this, Create1Activity::class.java)
+                    intent.putExtra(Create1Activity.user_login_cerbungHadiFamily, userId)
+                    startActivity(intent)
+                    true
+                }
+                R.id.itemUser -> {
+                    val intent = Intent(this, Users1Activity::class.java)
+                    intent.putExtra(Users1Activity.user_login_cerbungHadiFamily, userId)
+                    startActivity(intent)
+                    true
+                }
+                R.id.itemPref -> {
+                    true
+                }
+                else -> false
             }
-            R.id.item_notification -> {
-                // Tindakan yang ingin dilakukan saat menu diklik
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
         }
     }
-
+    override fun onBackPressed() {
+        binding.bottomNav.selectedItemId = R.id.itemHome
+    }
 }
